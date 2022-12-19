86425816c637ea9108da1ba6161f2c2ed3825c41
company window <jinpyolee19@naver.com>
2022-12-19 18:01:49 +0900

    컴포넌트 스캔

---
+++ spring-workspace/김영한/스프링입문/hello-spring/src/main/java/com/example/hellospring/service/MemberService.java
@@ -0,0 +1,55 @@
+package com.example.hellospring.service;
+
+import com.example.hellospring.domain.Member;
+import com.example.hellospring.repository.MemberRepository;
+import com.example.hellospring.repository.MemoryMemberRepositry;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Service: Service 스러운, 사람 친화적인 용어
+ */
+
+@Service
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+
+    // repository의 실제 구현체를 찾음
+    @Autowired
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    /**
+     * 회원가입
+     */
+    public Long join(Member member) {
+        // 같은 이름있는 중복회원 x
+        validateDuplicateMember(member);
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
