06d42397cc53ad761a122ce47b17f56aacb7104e
leejp <jinpyolee19@naver.com>
2023-01-02 16:43:56 +0900

    할인정책 %로 변경

---
+++ spring-workspace/김영한/스프링핵심원리-기본편/core/src/main/java/spring/core/OrderApp.java
@@ -0,0 +1,26 @@
+package spring.core;
+
+import spring.core.member.Grade;
+import spring.core.member.Member;
+import spring.core.member.MemberService;
+import spring.core.member.MemberServiceImpl;
+import spring.core.order.Order;
+import spring.core.order.OrderService;
+import spring.core.order.OrderServiceImpl;
+
+public class OrderApp {
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        OrderService orderService = new OrderServiceImpl();
+
+        long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+
+        // toString() 호출
+        System.out.println("order = " + order);
+        System.out.println("order.calculatePrice() = " + order.calculatePrice());
+    }
+}
