625ce4437e57b107e01014d57833276b1b2f1daf
leejp <jinpyolee19@naver.com>
2023-01-03 11:11:51 +0900

    관심사의 분리 완료

--- spring-workspace/김영한/스프링핵심원리-기본편/core/src/main/java/spring/core/member/MemberServiceImpl.java
+++ spring-workspace/김영한/스프링핵심원리-기본편/core/src/main/java/spring/core/member/MemberServiceImpl.java
@@ -7,9 +7,18 @@ import spring.core.member.MemoryMemberRepository;
 
 public class MemberServiceImpl implements MemberService {
     
-    // memberRepository가 실제 할당할 때 구현체에 의존을 함 -> MemberServiceImpl는 추상화와 구현체 모두 의존 해버림 -> DIP 위반
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    /*memberRepository가 실제 할당할 때 구현체에 의존을 함 -> MemberServiceImpl는 추상화와 구현체 모두 의존 해버림 -> DIP 위반*/
+    // private final MemberRepository memberRepository = new MemoryMemberRepository();
+    
+    /*단지, 인터페이스에 의존만 함*/
+    private final MemberRepository memberRepository;
+    
+    /*생성자를 통해 주입 -> 구현체에 대한 코드는 어디에도 없음 -> DIP(추상화에만 의존) 지킴*/
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
+    /*구현체에 의존하지 않게 되면서 실행에만 집중 -> 관심사의 분리 완수*/
     @Override
     public void join(Member member) {
         memberRepository.save(member);
--- spring-workspace/김영한/스프링핵심원리-기본편/core/src/main/java/spring/core/order/OrderServiceImpl.java
+++ spring-workspace/김영한/스프링핵심원리-기본편/core/src/main/java/spring/core/order/OrderServiceImpl.java
@@ -2,17 +2,39 @@ package spring.core.order;
 
 import spring.core.discount.DiscountPolicy;
 import spring.core.discount.FixDiscountPolicy;
+import spring.core.discount.RateDiscountPolicy;
 import spring.core.member.Member;
 import spring.core.member.MemberRepository;
 import spring.core.member.MemoryMemberRepository;
 
+/*실질적인 할인 정책 클라이언트*/
+/*DIP위반-> 클라이언트가 추상화에 의존하지만, 구체화에도 의존중( = new ...)*/
+/*여기서 사용하는 필드는 2개 */
 public class OrderServiceImpl implements OrderService {
-    // 어떤 회원이 주문 했는가?
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    /*어떤 회원이 주문 했는가?*/
+    // private final MemberRepository memberRepository = new MemoryMemberRepository();
+    /*DIP를 지키기 위해 인터페이스만 선언*/
+    /* * final키워드: 생성자를 통해 할당이 되야 함*/
+    private final MemberRepository memberRepository;
 
-    // 어떤 할인정책을 사용 할 것인가?
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    
+    /*어떤 할인정책을 사용 할 것인가?*/
+    /*마치 로미오 배역을 맡은 디카프리오가 줄리엣 역할을 할 배우를 직접 캐스팅 하는 느낌*/
+    //private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
 
+    /*할인 정책을 바꾸려면? 코드의 수정이 일어날 수 밖에 없는 구조*/
+    //private final DiscountPolicy rateDiscountPolicy = new RateDiscountPolicy();
+
+    /*DIP 만족(추상에만 의존하도록 변경) -> Null 값 -> 누군가 구현객체를 대신 생성하고 주입 -> AppConfig 등장(감독)*/
+    private final DiscountPolicy discountPolicy;
+
+    /*생성자 주입을 통해 DIP 원칙 지키기 -> AppConfig에서 어떤거 사용할지 정함*/
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
+
+    /*구현체에 의존하지 않게 되면서 실행에만 집중 -> 관심사의 분리 완수*/
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member = memberRepository.findById(memberId);
