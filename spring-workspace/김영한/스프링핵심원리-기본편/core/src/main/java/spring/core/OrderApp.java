1ca90bd54f2596e89131bc798947b04586297cb9
leejp <jinpyolee19@naver.com>
2023-01-03 14:59:33 +0900

    JAVA코드->Spring을 사용한 코드로 변경

--- spring-workspace/김영한/스프링핵심원리-기본편/core/src/main/java/spring/core/OrderApp.java
+++ spring-workspace/김영한/스프링핵심원리-기본편/core/src/main/java/spring/core/OrderApp.java
@@ -1,5 +1,7 @@
 package spring.core;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import spring.core.member.Grade;
 import spring.core.member.Member;
 import spring.core.member.MemberService;
@@ -24,17 +26,33 @@ public class OrderApp {
 //        System.out.println("order.calculatePrice() = " + order.calculatePrice());
 
         /*DIP 를 잘 지킨 코드로 테스트*/
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+//        OrderService orderService = appConfig.orderService();
+
+//        long memberId = 1L;
+//        Member member = new Member(memberId, "memberA", Grade.VIP);
+//        memberService.join(member);
+
+//        Order order = orderService.createOrder(memberId, "itemA", 20000);
+
+        /*toString() 호출*/
+//        System.out.println("order = " + order);
+//        System.out.println("order.calculatePrice() = " + order.calculatePrice());
+
+        /*---Spring을 사용하여 작성---*/
+        /*ApplicationContext: Spring컨테이너 -> 스프링 Bean 관리 -> AppConfig에서 @붙은거 관리 -> 스프링 컨테이너에서 Bean객체로 생성하여 관리*/
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        /*메소드 명, 리턴타입*/
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
         long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
         memberService.join(member);
 
-        Order order = orderService.createOrder(memberId, "itemA", 10000);
-
-        // toString() 호출
+        Order order = orderService.createOrder(memberId, "itemA", 20000);
         System.out.println("order = " + order);
         System.out.println("order.calculatePrice() = " + order.calculatePrice());
     }
