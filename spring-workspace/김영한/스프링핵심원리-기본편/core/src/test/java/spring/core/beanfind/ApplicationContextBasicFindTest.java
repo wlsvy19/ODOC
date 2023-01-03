f5c219a86276a3ebf9c6754f00c88f3ae745ab15
leejp <jinpyolee19@naver.com>
2023-01-03 17:39:35 +0900

    스프링 빈 조회

---
+++ spring-workspace/김영한/스프링핵심원리-기본편/core/src/test/java/spring/core/beanfind/ApplicationContextBasicFindTest.java
@@ -0,0 +1,57 @@
+package spring.core.beanfind;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import spring.core.AppConfig;
+import spring.core.member.MemberService;
+import spring.core.member.MemberServiceImpl;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+/*스프링 빈 조회 테스트*/
+public class ApplicationContextBasicFindTest {
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("빈 이름으로 조회")
+    void findBeanByName() {
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        System.out.println("memberService = " + memberService);
+        System.out.println("memberService.getClass() = " + memberService.getClass());
+
+        /*검증 org.assertj*/
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("이름없이 타입으로만 조회")
+    void findBeanByType() {
+        MemberService memberService = ac.getBean(MemberService.class);
+
+        /*검증 org.assertj*/
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("구체 타입으로 조회")
+    void findBeanByName2() {
+        MemberService memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        /*검증 - org.assertj*/
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("빈 이름으로 조회X")
+    void findBeanByNameX() {
+        // MemberService memberService = ac.getBean("xxxx", MemberServiceImpl.class);
+
+        /* org.junit*/
+        /* 람다활용, -> 기점으로, 오른쪽 코드실행 시 왼쪽 예외가 터져야 테스트 성공 */
+        /*예외가 던져지는 테스트(Throws)*/
+        assertThrows(NoSuchBeanDefinitionException.class, () -> ac.getBean("xxxx", MemberServiceImpl.class));
+    }
+}
