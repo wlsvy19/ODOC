09a0d692f68325d8e74fc05876090e088719fc4b
leejp <jinpyolee19@naver.com>
2023-01-05 23:25:49 +0900

    component scan & @Autowired

--- spring-workspace/김영한/스프링핵심원리-기본편/core/src/main/java/spring/core/member/MemberServiceImpl.java
+++ spring-workspace/김영한/스프링핵심원리-기본편/core/src/main/java/spring/core/member/MemberServiceImpl.java
@@ -1,10 +1,13 @@
 package spring.core.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import spring.core.member.Member;
 import spring.core.member.MemberRepository;
 import spring.core.member.MemberService;
 import spring.core.member.MemoryMemberRepository;
 
+@Component
 public class MemberServiceImpl implements MemberService {
     
     /*memberRepository가 실제 할당할 때 구현체에 의존을 함 -> MemberServiceImpl는 추상화와 구현체 모두 의존 해버림 -> DIP 위반*/
@@ -12,8 +15,13 @@ public class MemberServiceImpl implements MemberService {
     
     /*단지, 인터페이스에 의존만 함*/
     private final MemberRepository memberRepository;
-    
+
+
     /*생성자를 통해 주입 -> 구현체에 대한 코드는 어디에도 없음 -> DIP(추상화에만 의존) 지킴*/
+
+    /*@Autowired: AppConfig에선 @Bean 붙은걸 찾았지만, AutoConfig에서 사용하기 위해서 의존관계를 자동으로 주입 시켜줌*/
+    /*마치 ac.getBean(MemberRepository.class) 처럼*/
+    @Autowired
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
