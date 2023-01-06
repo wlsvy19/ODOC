2aef59fe396685570d48d62c29b7312540d7433c
leejp <jinpyolee19@naver.com>
2023-01-06 11:29:09 +0900

    component scan filter

--- spring-workspace/김영한/스프링핵심원리-기본편/core/src/main/java/spring/core/AutoAppConfig.java
+++ spring-workspace/김영한/스프링핵심원리-기본편/core/src/main/java/spring/core/AutoAppConfig.java
@@ -9,8 +9,11 @@ import org.springframework.context.annotation.FilterType;
 
 /*@Component붙은 클래스를 찾아 자동으로 스프링 빈에 등록 시킴*/
 @ComponentScan(
-        /*뺄거 지정 -> 여기서 AppConfig 클래스가 @Configuration 붙어 있어서 빼줄라고 지정 */
-        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+        /*뺄거 지정 -> 원활한 테스트를 위해 @Configuration 들어간 모든 클래스를 제외 함 */
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class),
+        
+        /*어디부터 스캔 할 건지 지정 -> 지정 하지 않으면?? @ComponentScan붙은 클래스가 속한 패키지 부터 스캔함( spring.core)*/
+        basePackages = "spring.core"
 )
 public class AutoAppConfig {
 
