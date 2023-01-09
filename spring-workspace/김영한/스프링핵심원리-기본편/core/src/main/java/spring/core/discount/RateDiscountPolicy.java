c19e213519251cb3be3260f58401f1b4e9f9d7b5
leejp <jinpyolee19@naver.com>
2023-01-09 17:48:40 +0900

    의존관계 주입까지 끝

--- spring-workspace/김영한/스프링핵심원리-기본편/core/src/main/java/spring/core/discount/RateDiscountPolicy.java
+++ spring-workspace/김영한/스프링핵심원리-기본편/core/src/main/java/spring/core/discount/RateDiscountPolicy.java
@@ -1,10 +1,16 @@
 package spring.core.discount;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
+import spring.core.annotation.MainDiscountPolicy;
 import spring.core.member.Grade;
 import spring.core.member.Member;
 
 @Component
+// @Qualifier("mainDiscountPolicy") /*빈 이 여러개일 때 사용 -> @Autowired에서 명시하여 사용*/
+// @Primary  /*빈 이 여러개일 때 사용 -> 우선순위 지정*/
+@MainDiscountPolicy /*직접만든 어노테이션으로 하기*/
 public class RateDiscountPolicy implements DiscountPolicy {
     private int discountPercent = 10;
 
--- spring-workspace/김영한/스프링핵심원리-기본편/core/src/test/java/spring/core/scan/AutoAppConfigTest.java
+++ spring-workspace/김영한/스프링핵심원리-기본편/core/src/test/java/spring/core/scan/AutoAppConfigTest.java
@@ -4,7 +4,9 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import spring.core.AutoAppConfig;
+import spring.core.member.MemberRepository;
 import spring.core.member.MemberService;
+import spring.core.order.OrderServiceImpl;
 
 /*@Component가 붙은 클래스를 진짜로 찾아서 스프링 컨테이너에 스프링 빈으로 등록 되는지 테스트*/
 public class AutoAppConfigTest {
@@ -13,5 +15,9 @@ public class AutoAppConfigTest {
         AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class);
         MemberService memberService = ac.getBean(MemberService.class);
         Assertions.assertThat(memberService).isInstanceOf(MemberService.class);
+
+        OrderServiceImpl bean = ac.getBean(OrderServiceImpl.class);
+        MemberRepository memberRepository = bean.getMemberRepository();
+        System.out.println("memberRepository = " + memberRepository);
     }
 }
