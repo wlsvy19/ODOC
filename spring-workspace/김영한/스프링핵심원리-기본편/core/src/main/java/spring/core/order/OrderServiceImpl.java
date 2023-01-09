0ec3bbf652f86353075cb426257d6cb30b7987ad
leejp <jinpyolee19@naver.com>
2023-01-09 17:48:40 +0900

    의존관계 주입까지 끝

--- spring-workspace/김영한/스프링핵심원리-기본편/core/src/main/java/spring/core/order/OrderServiceImpl.java
+++ spring-workspace/김영한/스프링핵심원리-기본편/core/src/main/java/spring/core/order/OrderServiceImpl.java
@@ -1,7 +1,10 @@
 package spring.core.order;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
+import spring.core.annotation.MainDiscountPolicy;
 import spring.core.discount.DiscountPolicy;
 import spring.core.member.Member;
 import spring.core.member.MemberRepository;
@@ -12,11 +15,13 @@ import spring.core.member.MemberRepository;
 /*DIP위반-> 클라이언트가 추상화에 의존하지만, 구체화에도 의존중( = new ...)*/
 /*여기서 사용하는 필드는 2개 */
 @Component
+/*final이 붙은 필드를 가지고 생성자를 자동으로 만들어 줌*/
+//@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
     /*어떤 회원이 주문 했는가?*/
     // private final MemberRepository memberRepository = new MemoryMemberRepository();
     /*DIP를 지키기 위해 인터페이스만 선언*/
-    /* !final키워드: 생성자를 통해 할당이 되야 함*/
+    /* !final키워드: 초기값 넣어주거나 생성자를 통해 할당이 되야 함*/
     private final MemberRepository memberRepository;
 
     
@@ -31,11 +36,13 @@ public class OrderServiceImpl implements OrderService {
     private final DiscountPolicy discountPolicy;
 
     /*생성자 주입을 통해 DIP 원칙 지키기 -> AppConfig에서 실행시 어떤거 사용할지 정함 -> 동적인 객체 인스턴스 의존 관계*/
+    /*롬복의 @RequiredArgsConstructor로 대체 가능*/
     @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+    public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy) {
         /*생성자주입 으로 인해 스프링 컨테이너에 빈으로 자동 등록 됨*/
         System.out.println("memberRepository = " + memberRepository);
         System.out.println("discountPolicy = " + discountPolicy);
+
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
