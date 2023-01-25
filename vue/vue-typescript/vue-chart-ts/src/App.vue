dc5b3524ebfb9f43e412588a10a23ca512040b9b
leejp <jinpyolee19@naver.com>
2023-01-25 17:45:31 +0900

    chart.js error solve

---
+++ vue/vue-typescript/vue-chart-ts/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <canvas id="myChart"></canvas>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import { ChartConfiguration } from "chart.js";
+import Chart from "chart.js/auto";
+
+export default Vue.extend({
+  mounted() {
+    const labels = ["January", "February", "March", "April", "May", "June"];
+    const data = {
+      labels: labels,
+      datasets: [
+        {
+          label: "My First dataset",
+          backgroundColor: "rgb(255, 99, 132)",
+          borderColor: "rgb(255, 99, 132)",
+          data: [0, 10, 5, 2, 20, 30, 45],
+        },
+      ],
+    };
+    const config: ChartConfiguration = {
+      type: "line",
+      data,
+      options: {},
+    };
+    const ctx = (
+      document.getElementById("myChart") as HTMLCanvasElement
+    ).getContext("2d") as CanvasRenderingContext2D;
+
+    var myChart = new Chart(ctx, config);
+  },
+});
+</script>
