11bc2e72d32ea549d8a180c07a9dcf04aea9a35a
leejp <jinpyolee19@naver.com>
2023-01-27 16:52:27 +0900

    vue endgame_ ~register form

--- vue/vue-endgame/vue-til/src/App.vue
+++ vue/vue-endgame/vue-til/src/App.vue
@@ -1,14 +1,16 @@
 <template>
   <div>
-    <!-- <header>
+    <!-- 
+      <header>
       <router-link to="/login">로그인</router-link> |
       <router-link to="/signup">회원가입</router-link>
-    </header> -->
+    </header> 
+  -->
 
-    <app-header></app-header>
+    <AppHeader></AppHeader>
 
-    <!--routes/index.js 에서 정의한 path시에 보여지는 컴포넌트-->
-    <router-view></router-view>
+    <!--routes/index.js 에서 정의한 path에서 보여지는 실질적인 컴포넌트-->
+    <RouterView></RouterView>
   </div>
 </template>
 
@@ -16,7 +18,6 @@
 import AppHeader from '@/components/common/AppHeader.vue';
 
 export default {
-  name: 'app',
   components: {
     AppHeader,
   },
