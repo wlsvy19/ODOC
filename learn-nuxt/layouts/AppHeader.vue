eb33eedf9c7d28d38baf79d762a8a77a10897149
leejp <jinpyolee19@naver.com>
2023-02-10 15:57:15 +0900

    search keyword use props & emit

---
+++ learn-nuxt/layouts/AppHeader.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <header>
+      <header class="title">
+        <!-- <router-link> 와 동일-->
+        <NuxtLink to="/" class="logo">Nuxt</NuxtLink>
+      </header>
+      <nuxt-link to="main" class="logo">메인</nuxt-link>
+      <Nuxt-Link to="/board" class="logo">게시판 </Nuxt-Link>
+      <NuxtLink to="/register" class="logo"> 회원가입</NuxtLink>
+      <NuxtLink to="/login" class="logo"> 로그인</NuxtLink>
+    </header>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+/* 헤더 관련 스타일 */
+header {
+  height: 70px;
+  display: flex;
+  align-items: center;
+  padding: 0 0.5rem;
+}
+.logo {
+  font-weight: 900;
+  margin-left: 100px;
+}
+.logo:visited {
+  color: inherit;
+}
+</style>
