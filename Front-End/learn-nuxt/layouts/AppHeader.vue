59f956820251c1d47f1a08be1893da48828f7d76
leejp <jinpyolee19@naver.com>
2023-02-16 09:26:13 +0900

    front end directory structure organizing

---
+++ Front-End/learn-nuxt/layouts/AppHeader.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <header>
+      <header class="title">
+        <!-- <router-link> 와 동일-->
+        <NuxtLink to="/" class="logo">Nuxt</NuxtLink>
+      </header>
+      <div class="munu">
+        <Nuxt-Link to="/board" class="logo">게시판 </Nuxt-Link>
+        <NuxtLink to="/register" class="logo"> 회원가입</NuxtLink>
+        <nuxtLink to="/login" class="logo"> 로그인</nuxtLink>
+      </div>
+    </header>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+/* 헤더 관련 스타일 */
+header {
+  height: 70px;
+  display: flex;
+  align-items: center;
+  padding: 0 0.5rem;
+}
+.logo {
+  font-weight: 900;
+  margin-left: 100px;
+}
+.logo:visited {
+  color: inherit;
+}
+</style>
---
+++ Front-End/learn-nuxt/nuxt.config.js
@@ -0,0 +1,56 @@
+export default {
+  // Target: https://go.nuxtjs.dev/config-target
+  // target: 'static',
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'nuxt',
+    htmlAttrs: {
+      lang: 'en',
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  // * 전역 css 설정
+  css: ['@/assets/css/reset.css'],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/eslint
+    '@nuxtjs/eslint-module',
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [],
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {},
+
+  // * 서버포트 변경: https://nuxtjs.org/docs/features/configuration/#edit-host-and-port
+  // process.env.NODE_ENV: nuxt에서 제공하는 환경변수
+  // 개발일땐 4000포트, 배포일땐 null
+  // 개발: development
+  server: {
+    port: process.env.NODE_ENV === 'production' ? null : 4000,
+  },
+
+  // * env
+  env: {
+    baseURL:
+      process.env.NODE_ENV === 'production'
+        ? 'https://my-json-server.typicode.com/joshua1988/nuxt-shopping-api'
+        : 'http://localhost:3000',
+  },
+}
