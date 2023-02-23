3b9038d871cb6067293ec70ff900b99f54ec3cf3
leejp <jinpyolee19@naver.com>
2023-02-23 10:33:27 +0900

    Servlet, JSP, MVC

--- spring-workspace/김영한/3. 스프링MVC1/servlet/src/main/java/mvc1/servlet/web/servletmvc/MvcMemberListServlet.java
+++ spring-workspace/김영한/3. 스프링MVC1/servlet/src/main/java/mvc1/servlet/web/servletmvc/MvcMemberListServlet.java
@@ -10,26 +10,18 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
-@WebServlet(name="mvcMemberSaveServlet", urlPatterns = "/servlet-mvc/members/save")
-public class MvcMemberSaveServlet extends HttpServlet {
+@WebServlet(name = "mvcMemberListServlet", urlPatterns = "/servlet-mvc/members")
+public class MvcMemberListServlet extends HttpServlet {
     private final MemberRepository memberRepository = MemberRepository.getInstance();
 
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // 1. 비즈니스 로직 실행
-        String username = request.getParameter("username");
+        List<Member> members = memberRepository.findAll();
+        request.setAttribute("members", members);
 
-        int age = Integer.parseInt(request.getParameter("age"));
-
-        Member member = new Member(username, age);
-        memberRepository.save(member);
-
-        // 2. Model에 데이터 보관
-        // request내부에 저장소 있음
-        request.setAttribute("member", member);
-
-        String viewPath = "/WEB-INF/views/save-result.jsp";
+        String viewPath = "/WEB-INF/views/members.jsp";
         RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
         dispatcher.forward(request, response);
     }
