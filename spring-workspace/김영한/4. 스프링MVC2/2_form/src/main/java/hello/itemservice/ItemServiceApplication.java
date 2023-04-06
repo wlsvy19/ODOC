89c8d1f28382de078fe1e695cd6bcbe7592e4778
leejp <jinpyolee19@naver.com>
2023-04-06 15:39:37 +0900

    메시지, 국제화 까지 끝

--- spring-workspace/김영한/4. 스프링MVC2/2_form/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ spring-workspace/김영한/4. 스프링MVC2/2_form/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -3,6 +3,10 @@ package hello.itemservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 타임리프 기본 기능을 다루는 프로젝트
+ * 포트번호: 8086
+ */
 @SpringBootApplication
 public class ItemServiceApplication {
 
