24d99cffa2554c2f69e3fc52188b3b9ea1470e9f
leejp <jinpyolee19@naver.com>
2023-10-18 18:00:48 +0900

    API 예외처리

---
+++ spring-workspace/김영한/4. 스프링MVC2/6_exception/src/main/java/hello/exception/exception/UserException.java
@@ -0,0 +1,14 @@
+package hello.exception.exception;
+
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MyHandlerExceptionResolver implements HandlerExceptionResolver {
+	@Override
+	public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
+		return null;
+	}
+}
