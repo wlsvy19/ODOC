fe950946769799d19011d9ea1fa9f384c8e3703d
leejp <jinpyolee19@naver.com>
2023-11-16 17:20:53 +0900

    JPA 다양한  쿼리 메서드 사용 예

--- spring-workspace/AllinOne/Part4-1_Spring-jpa/bookmanager/src/main/java/com/fastcampus/jpa/bookmanager/repository/UserRepository.java
+++ spring-workspace/AllinOne/Part4-1_Spring-jpa/bookmanager/src/main/java/com/fastcampus/jpa/bookmanager/repository/UserRepository.java
@@ -1,8 +1,10 @@
 package com.fastcampus.jpa.bookmanager.repository;
 
+import com.fastcampus.jpa.bookmanager.domain.Address;
 import com.fastcampus.jpa.bookmanager.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Set;
 
@@ -34,10 +36,55 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
 	// 쿼리에 LIMIT 1 붙음
 	List<User> findFirst1ByName(String name);
+
 	List<User> findFirst2ByName(String name);
 
 	// 쿼리에 LIMIT 붙음
 	List<User> findTop1ByName(String name);
 
 	List<User> findLast1ByName(String name);
+
+	// WHERE 조건에 AND
+	List<User> findByEmailAndName(String email, String name);
+
+	List<User> findByEmailOrName(String email, String name);
+
+	// WHERE 조건에 부등호 >, 이후에 만들어진 데이터
+	List<User> findByCreatedAtAfter(LocalDateTime yesterday);
+
+	// WHERE 조건에 부등호 <, 이전에 만들어진 데이터
+	List<User> findByUpdatedAtBefore(LocalDateTime tomorrow);
+
+	// >
+	List<User> findByIdAfter(Long id);
+
+	// >
+	List<User> findByCreatedAtGreaterThan(LocalDateTime yesterday);
+
+	// >=
+	List<User> findByCreatedAtGreaterThanEqual(LocalDateTime yesterday);
+
+	// Between
+	List<User> findByCreatedAtBetween(LocalDateTime yesterday, LocalDateTime tomorrow);
+
+	List<User> findByIdBetween(Long id1, Long id2);
+
+	// Between 풀어쓴 메서드
+	List<User> findByIdGreaterThanEqualAndIdLessThanEqual(Long id1, Long id2);
+
+	// user0_.id is not null
+	List<User> findByIdIsNotNull();
+
+	// List<Address> findByAddressIsNotEmpty();
+
+	List<User> findByNameIn(List<String> names);
+
+	List<User> findByNameLike(String name);
+	List<User> findByNameStartingWith(String name);
+	List<User> findByNameEndingWith(String name);
+	List<User> findByNameContains(String name);
+
+	List<User> findUserByNameIs(String name);
+	List<User> findUserByName(String name);
+	List<User> findUserByNameEquals(String name);
 }
