71d5e9b435b14fb468b82adae86f4de627150ba0
leejp <jinpyolee19@naver.com>
2024-10-14 09:31:43 +0900

    코테 프로젝트 생성

---
+++ java-workspace/김영한/java-start/src/variable/Var3.java
@@ -0,0 +1,12 @@
+package variable;
+
+// 변수 변경
+public class Var3 {
+	public static void main(String[] args) {
+		int a; // 변수 선언
+		a = 10;
+		System.out.println("a = " + a);
+		a = 50;
+		System.out.println("a = " + a);
+	}
+}
---
+++ java-workspace/김영한/java-start/src/variable/Var7.java
@@ -0,0 +1,24 @@
+package variable;
+
+// 변수 타입
+public class Var7 {
+
+	public static void main(String[] args) {
+		int a = 100; // 정수
+		double b = 10.5; // 실수
+		boolean c = true; //  불리언 true, false 입력 가능
+		char d = 'A'; // 문자 하나
+		String e = "Hello"; // 문자열을 다루기 위한 특별한 타입
+
+		System.out.println("a = " + a);
+		System.out.println("b = " + b);
+		System.out.println("c = " + c);
+		System.out.println("d = " + d);
+		System.out.println("e = " + e);
+		
+		/*
+		 리터럴: 개발자가 직접 넣은 고정된 값
+		 */
+	}
+
+}
