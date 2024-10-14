33f8b81f35d836197e22b9c520be53cdaea37ac8
leejp <jinpyolee19@naver.com>
2024-10-14 11:20:45 +0900

    스택 문제 추가

---
+++ cote/src/five/ArrayControl.java
@@ -0,0 +1,13 @@
+package five;
+
+/**
+ * Step 1
+ * 정수 배열을 하나 받는다. 배열의 중복값을 제거하고 배열 데이터를 내림차순으로 정렬해서 반환
+ * 조건1. 배열 길이는 2이상 1000 이하
+ * 조건2. 각 배열의 데이터 값은 -100000 이상 100000 이하
+ */
+public class ArrayControl {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ cote/src/six/DecimalConversion.java
@@ -0,0 +1,17 @@
+package six;
+
+/**
+ * Step 2
+ * 진수변환
+ * 10진수를 입력 받아 2진수로 변환해 반환하는 solution() 함수 구현
+ * 제약조건: decimal은 1이상 10억 미만의 자연수
+ */
+public class DecimalConversion {
+    public static void main(String[] args) {
+        System.out.println(solution(1002345));
+    }
+
+    private static int solution(int num) {
+        return num;
+    }
+}
