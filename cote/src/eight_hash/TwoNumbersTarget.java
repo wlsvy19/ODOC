869c867c453482771bcc7612737cc5a84c08f684
leejp <jinpyolee19@naver.com>
2024-10-16 17:58:36 +0900

    두개의 숫자 타겟 문제

--- cote/src/eight_hash/TwoNumbersTarget.java
+++ cote/src/eight_hash/TwoNumbersTarget.java
@@ -2,18 +2,37 @@ package eight_hash;
 
 /**
  * [두 개의 수로 특정값 만들기]
- * n개의 양의 정수로 이루어진 배열 arr와 정수 target이 주어졌을 때 이 중에서 합이 target인 두 수가 arr에
- * 있는지 찾고, 있으면 true, 없으면 false를 반환하는 solution() 작성
- *
+ * n개의 양의 정수로 이루어진 배열 arr와 정수 target이 주어졌을 때
+ * 이 중에서 합이 target인 두 수가 arr에 있는지 찾고, 있으면 true, 없으면 false를 반환하는 solution() 작성
+ * <p>
  * 제약조건
  * 1. n은 2이상 10000 이하의 자연수
  * 2. arr의 각 원소는 1 이상 10000 이하의 자연수
  * 3. arr의 원소 중 중복되는 원소는 없음
  * 4. target은 1 이상 20000 이하의 자연수
- *
- *  arr             target          return
- *  [1,2,3,4,8]       6              true
- *  [2,3,5,9]         10            false
+ * <p>
+ * arr             target          return
+ * [1,2,3,4,8]       6              true
+ * [2,3,5,9]         10            false
  */
 public class TwoNumbersTarget {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 8};
+        int target = 6;
+        System.out.println(solution(arr, target));
+    }
+
+    private static boolean solution(int[] arr, int target) {
+        boolean answer = false;
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+
+                if (arr[i] + arr[j] == target) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 }
