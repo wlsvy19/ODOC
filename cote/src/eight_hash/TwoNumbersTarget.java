9e597c115ed2de71617054e61961cbb79f43af57
leejp <jinpyolee19@naver.com>
2024-10-16 17:35:46 +0900

    해시 진행 중

---
+++ cote/src/eight_hash/TwoNumbersTarget.java
@@ -0,0 +1,19 @@
+package eight_hash;
+
+/**
+ * [두 개의 수로 특정값 만들기]
+ * n개의 양의 정수로 이루어진 배열 arr와 정수 target이 주어졌을 때 이 중에서 합이 target인 두 수가 arr에
+ * 있는지 찾고, 있으면 true, 없으면 false를 반환하는 solution() 작성
+ *
+ * 제약조건
+ * 1. n은 2이상 10000 이하의 자연수
+ * 2. arr의 각 원소는 1 이상 10000 이하의 자연수
+ * 3. arr의 원소 중 중복되는 원소는 없음
+ * 4. target은 1 이상 20000 이하의 자연수
+ *
+ *  arr             target          return
+ *  [1,2,3,4,8]       6              true
+ *  [2,3,5,9]         10            false
+ */
+public class TwoNumbersTarget {
+}
