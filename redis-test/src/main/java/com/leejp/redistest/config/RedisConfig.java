802be94b7561e99a091a25c76cb401d78953fcec
jinpyolee19 <jinpyolee19@naver.com>
2024-12-11 17:14:09 +0900

    redis 적용 방법 추가

--- redis-test/src/main/java/com/leejp/redistest/config/RedisConfig.java
+++ redis-test/src/main/java/com/leejp/redistest/config/RedisConfig.java
@@ -2,14 +2,45 @@ package com.leejp.redistest.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.Map;
 
 
 @Configuration
 public class RedisConfig {
+
+//    @Bean
+//    public RedisConnectionFactory redisConnectionFactory() {
+//        return new LettuceConnectionFactory("localhost", 6379);
+//    }
+
+    // 이
     @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory("localhost", 6379);
+    public RedisCacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+        RedisCacheConfiguration cacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(10)); // 캐시 만료 시간 설정 (10분) <- 우선순위 2
+
+        return RedisCacheManager.builder(redisConnectionFactory)
+                .cacheDefaults(cacheConfiguration)
+                .build();
+    }
+
+    @Bean // 제네릭 타입은 내가 사용할 곳에 맞춰 해야 함
+    public RedisTemplate<String, List<Map<String, Object>>> redisTemplate(RedisConnectionFactory factory) {
+        RedisTemplate<String, List<Map<String, Object>>> template = new RedisTemplate<>();
+        template.setConnectionFactory(factory);
+        // 사람이 읽을 수 있도록 직렬화 설정 추가
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        return template;
     }
 }
