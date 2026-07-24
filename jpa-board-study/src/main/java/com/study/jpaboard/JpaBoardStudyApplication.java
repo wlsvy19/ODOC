8e2e40c86f65ccc60efd3b5e1fd888c08321a14b
leejp <wlsvy19@gmail.com>
2026-07-24 13:23:05 +0900

    JPA CURD study를 위한 프로젝트 생성

---
+++ jpa-board-study/src/main/java/com/study/jpaboard/JpaBoardStudyApplication.java
@@ -0,0 +1,13 @@
+package com.study.jpaboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaBoardStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaBoardStudyApplication.class, args);
+	}
+
+}
---
+++ jpa-board-study/src/test/java/com/study/jpaboard/JpaBoardStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.jpaboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaBoardStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
