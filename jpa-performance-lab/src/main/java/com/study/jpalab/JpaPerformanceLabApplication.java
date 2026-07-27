d6fa9b1ce8ec83490e039505d3e994b364bc94af
leejp <wlsvy19@gmail.com>
2026-07-27 14:15:11 +0900

    jpa-performance-lab_JSP 실행형 War 환경 구성

--- jpa-performance-lab/src/main/java/com/study/jpalab/JpaPerformanceLabApplication.java
+++ jpa-performance-lab/src/main/java/com/study/jpalab/JpaPerformanceLabApplication.java
@@ -2,7 +2,10 @@ package com.study.jpalab;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+/*
 @SpringBootApplication
 public class JpaPerformanceLabApplication {
 
@@ -10,4 +13,18 @@ public class JpaPerformanceLabApplication {
 		SpringApplication.run(JpaPerformanceLabApplication.class, args);
 	}
 
+}
+
+*/
+@SpringBootApplication
+public class JpaPerformanceLabApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(JpaPerformanceLabApplication.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaPerformanceLabApplication.class, args);
+	}
 }
